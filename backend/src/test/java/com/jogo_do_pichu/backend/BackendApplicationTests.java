04115277403a3639e1141acfa914f846d41fe022
Lucas Ferreira <Lucasfdasilva2002@gmail.com>
2026-02-24 14:23:29 -0400

    inserindo arquivos base do spring

---
+++ backend/src/test/java/com/jogo_do_pichu/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jogo_do_pichu.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
