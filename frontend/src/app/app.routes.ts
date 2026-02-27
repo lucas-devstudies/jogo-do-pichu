dfcb9d0f2c405db416b11b65a7968623f7274d39
Lucas Ferreira da Silva <87401472+lucas-devstudies@users.noreply.github.com>
2026-02-27 15:47:59 -0400

    Revert "31 construir tela inicial no angular"

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,3 @@
 import { Routes } from '@angular/router';
-import { Principal } from './features/principal/principal';
 
-export const routes: Routes = [
-    {path:'',component:Principal},
-    {path:'home',component:Principal},
-];
+export const routes: Routes = [];
