1be575fb47500d07dba300290b830fde73362e3b
Lucas Ferreira <Lucasfdasilva2002@gmail.com>
2026-03-03 14:36:45 -0400

    inserindo correções no scrool das cidades

--- frontend/src/app/features/principal/cidades/cidades.ts
+++ frontend/src/app/features/principal/cidades/cidades.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, Input, Output, QueryList, ViewChildren } from '@angular/core';
 import { Button } from "../../../core/components/button/button";
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -12,14 +12,22 @@ import { CustomInput } from "../../../shared/components/custom-input/custom-inpu
   templateUrl: './cidades.html',
   styleUrl: './cidades.css',
 })
-export class Cidades {
+export class Cidades implements AfterViewInit {
   @Input()
   extraClass = '';
 
-  cities;
+  @ViewChildren('scrollContainer') scrollContainers!: QueryList<ElementRef>;
+
+  ngAfterViewInit(): void {
+  this.configurarScroll(); 
+  this.scrollContainers.changes.subscribe(() => {
+    this.configurarScroll();
+  });
+}
+  regions;
   valor:BigInt = 20n;
   constructor(private pokeapiService:PokeapiService){
-    this.cities = this.pokeapiService.regions;
+    this.regions = this.pokeapiService.regions;
   }
 
   
@@ -29,5 +37,39 @@ export class Cidades {
 
   change(){
     this.opcaoSelecionada.emit(this.escolhida);
+  }
+  configurarScroll(){
+    this.scrollContainers.forEach((containerRef: ElementRef) => {
+    const container = containerRef.nativeElement;
+
+    let isDown = false;
+    let startX = 0;
+    let scrollLeft = 0;
+
+    container.addEventListener('mousedown', (e: MouseEvent) => {
+      isDown = true;
+      container.style.cursor = 'grabbing';
+      startX = e.pageX - container.offsetLeft;
+      scrollLeft = container.scrollLeft;
+      e.preventDefault();
+    });
+
+    container.addEventListener('mouseleave', () => {
+      isDown = false;
+      container.style.cursor = 'grab';
+    });
+
+    container.addEventListener('mouseup', () => {
+      isDown = false;
+      container.style.cursor = 'grab';
+    });
+
+    container.addEventListener('mousemove', (e: MouseEvent) => {
+      if (!isDown) return;
+      const x = e.pageX - container.offsetLeft;
+      const walk = x - startX;
+      container.scrollLeft = scrollLeft - walk;
+    });
+  });
   }
 }
