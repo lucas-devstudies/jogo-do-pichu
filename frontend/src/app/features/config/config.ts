76bbd888836bc2f33a58a73a199faed8a3edd8c5
Lucas Ferreira <Lucasfdasilva2002@gmail.com>
2026-03-05 11:20:04 -0400

    inserindo sobre

--- frontend/src/app/features/config/config.ts
+++ frontend/src/app/features/config/config.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Menu } from "../../core/components/menu/menu";
 import { TokenService } from '../../core/services/token-service';
 import { Router } from '@angular/router';
+import { Sobre } from './sobre/sobre';
 
 @Component({
   selector: 'app-config',
-  imports: [Menu],
+  imports: [Menu,Sobre],
   templateUrl: './config.html',
   styleUrl: './config.css',
 })
@@ -13,8 +14,13 @@ export class Config {
 
   constructor(private tokenService:TokenService,private router:Router){}
 
+  pagina:"sobre"| "config"= "config";
+
   logout(){
     this.tokenService.logout();
     this.router.navigate(['principal']);
+  }
+  toConfig(){
+    this.pagina="sobre";
   }
 }
